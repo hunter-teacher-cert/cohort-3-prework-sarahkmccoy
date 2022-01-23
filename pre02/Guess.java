5b9943824c41884a1ee9c93aa6f1e12d2a47b281
Sarah McCoy <96992739+sarahkmccoy@users.noreply.github.com>
2022-01-23 17:33:15 +0000

    trying to compile

--- pre02/Guess.java
+++ pre02/Guess.java
@@ -3,6 +3,18 @@ import java.util.*;
 
 public class Guess{
     public static void main(String[] args){
-        System.out.println("Hello world!");
+			//pick a random number
+				Random random = new Random();
+        int number = random.nextInt(100)+1;
+				int guess;
+				int error;
+				Scanner in = new Scanner(System.in);
+				System.out.println("I'm thinking of a number between 1 and 100 (inclusive).  Can you guess what it is? ");
+				System.out.println("Type a number:");
+				guess = in.nextInt();
+				System.out.println("Your guess is: " + guess);
+				error = abs(error - guess);
+				System.out.println("The number I was thinking of is: " + number);
+				System.out.println("You were off by: " + error);
     }
 }
