7a93d32712b6b7866ad9a85c9fc3f7fa01d27cd0
Sarah McCoy <96992739+sarahkmccoy@users.noreply.github.com>
2022-02-13 19:26:12 +0000

    Ex 8

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -6,6 +6,7 @@ public class Methods{
 	public static void main(String[] args){
     System.out.println(isDivisible(15,4));
 		System.out.println(isTriangle(3,4,8));
+		System.out.println(Ack(3,3));
 		}
 
   public static boolean isDivisible(int n, int m) {
@@ -28,4 +29,20 @@ public class Methods{
 		return true;
 		}
 	}
+
+ public static int Ack(int m, int n){
+	 if (m == 0){
+		 return n + 1;
+	 	}else if (m > 0 && n ==0){
+			return Ack(m - 1, 1);
+		}else {
+			return Ack(m-1, Ack(m, n-1));
+		}
+			
+ }
+
 }
+
+
+
+
