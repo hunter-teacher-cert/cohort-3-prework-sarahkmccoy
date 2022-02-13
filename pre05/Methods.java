feeb404af07c8fc5f048098730f3378602da7284
Sarah McCoy <96992739+sarahkmccoy@users.noreply.github.com>
2022-02-13 18:52:15 +0000

    added ex 2

---
+++ pre05/Methods.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Methods{
+	public static void main(String[] args){
+    System.out.println(isDivisible(15, 4));
+		}
+
+  public static boolean isDivisible(int n, int m) {
+			return(n % m == 0);
+		}
+        
+}	
