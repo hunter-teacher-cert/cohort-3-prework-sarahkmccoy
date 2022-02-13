22b141c70196ec034cd0a6f58dc6dd5c0a95f681
Sarah McCoy <96992739+sarahkmccoy@users.noreply.github.com>
2022-02-13 19:11:16 +0000

    added Ex 3

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -4,11 +4,28 @@ import java.util.*;
 
 public class Methods{
 	public static void main(String[] args){
-    System.out.println(isDivisible(15, 4));
+    System.out.println(isDivisible(15,4));
+		System.out.println(isTriangle(3,4,8));
 		}
 
   public static boolean isDivisible(int n, int m) {
 			return(n % m == 0);
 		}
         
-}	
+	
+	public static boolean isTriangle(int a, int b, int c) {
+	//I debated using a long or statement
+		if (a + b <= c) {
+			return false;
+		}
+		else if (b + c <= a) {
+		return false;
+		}
+		else if (a + c <= b) {
+			return false;
+		}
+		else {
+		return true;
+		}
+	}
+}
