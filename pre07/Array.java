574181334d07c01cffaf9bb55a26d33a8cbbf661
Sarah McCoy <96992739+sarahkmccoy@users.noreply.github.com>
2022-04-07 21:39:56 +0000

    Generalized for any power

--- pre07/Array.java
+++ pre07/Array.java
@@ -5,15 +5,18 @@ public class Array{
 	public static void main(String[] args) {
 	    
 		double[] a = {1.0,2.0,3.0,4.0};
+		double n = 3;
+		System.out.println("Here is an array:");
 		System.out.println(Arrays.toString(a));
-		powArray(a);
+		System.out.println("Here is that array raised to the power " + n);
+		powArray(a,n);
 		System.out.println(Arrays.toString(a));
 		
 	}
 
-	public static double[] powArray(double[] a){
+	public static double[] powArray(double[] a, double n){
 		for (int i = 0; i < a.length; i++) {
-    	a[i] = Math.pow(a[i], 2.0);
+    	a[i] = Math.pow(a[i], n);
 			}
 			return a;
 		}
